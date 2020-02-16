eba3ecc65b57d3dcbea8369d8259b8493ea089f9
kyragaut <59374890+kyragaut@users.noreply.github.com>
2020-02-16 11:57:17 -1000

    Update eth-secp256k1-hd.js

--- key-type/eth-secp256k1-hd.js
+++ key-type/eth-secp256k1-hd.js
@@ -1,6 +1,6 @@
 /*
 This is part of a key manager. 
 This part is responsible for generating a hierarchical deterministic (HD) key from a secret. 
-Secrets are generated from curves. 
+Secrets are generated from elliptic curves. 
 One important feature of is that it is not limited to using one specific curve. 
 */
